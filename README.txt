36c729853e3d82c0bad7f5367b9580e9ce56e846
mulhamzak <mulhamzak@gmail.com>
2025-01-20 20:32:08 +0300

    README.txt

---
+++ README.txt
@@ -0,0 +1,17 @@
+To open the project:
+    1. Please follow the following file path: P3_Assignment\src\GUIs\loginInterface\GetStarted.class
+    2. Run the main method in the file
+
+=======================================================================================================
+
+To try the manager interfaces, please enter the following data:
+    Email: Superadmin@Orange.com
+    Password: 0000
+
+To try the employee interfaces, please enter the following data:
+    Email:
+    Password:
+
+To try the customer interfaces, please enter the following data:
+    Email:
+    Password:
--- src/Backend logic/BackendMain.java
+++ src/Backend logic/BackendMain.java
@@ -7,13 +7,13 @@ public class BackendMain {
     public static void main (String[] args) throws IOException {
 //        System.out.println(MealsManagment.createMeal("Pizza", "Cheese", 25.0, "icons\\\\Pizza.png").getMsg());
 //        List<Meal> meals = MealsManagment.getMeals();
-//
+
 //        List<OrderItem> orderItems = new ArrayList<>();
 //        orderItems.add(new OrderItem(meals.get(0), 3));
 //        orderItems.add(new OrderItem(meals.get(1), 2));
-//
+
 //        System.out.println(OrderManagement.getOrdersEmployee());
-//
+
 //        System.out.println(OrderManagement.countOrdersForToday());
 //        System.out.println(OrderManagement.countOrdersForToday());
 //        System.out.println(OrderManagement.dailyRevenues());
